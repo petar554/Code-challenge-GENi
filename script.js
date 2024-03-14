995aebc91a8e88521c2230000e7fae47cd1dd0dd
Petar.Buric <petarburic554@gmail.com>
2024-03-14 22:17:35 +0100

    Puzzle task - first version

---
+++ script.js
@@ -0,0 +1,44 @@
+// v1
+function countContainingRanges(input) {
+    const pairs = input.split('\n');
+    console.log(pairs);
+    
+    let containingCount = 0;
+    let overlappingCount = 0;
+
+    for (const pair of pairs) {
+        const [range1, range2] = pair.split(',');
+
+        const [start1, end1] = range1.split('-').map(Number);
+        const [start2, end2] = range2.split('-').map(Number);
+
+        // if ((start1 <= start2 && end1 >= end2) || (start2 <= start1 && end2 >= end1)) {
+        //     containingCount++;
+        // }
+        
+        if ((start1 >= start2 && end1 <= end2) || (start2 >= start1 && end2 <= end1)) {
+            containingCount++;
+        }
+
+        // overlapping ranges
+        if (start1 < end2 && start2 < end1) {
+            overlappingCount++;
+        }
+    }
+
+    return { containingCount, overlappingCount };
+}
+
+const sampleInputData = [
+    '2-4,6-8',
+    '2-3,4-5',
+    '5-7,7-9',
+    '2-8,3-7',
+    '6-6,4-6',
+    '2-6,4-8',
+].join('\n');
+
+const result = countContainingRanges(sampleInputData);
+console.log("Containing Count:", result.containingCount);
+console.log("Overlapping Count:", result.overlappingCount);
+
